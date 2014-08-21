ed2261549acd0b54c2466eed1160cf9fdc2ac6f8
Chris Speck <chris@chrisspeck.com>
2014-08-21 17:42:11 +1000

    Implement dual canvases and most of the image/box drawing

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -85,30 +85,100 @@ angular.module('myApp.controllers', [])
         ctx.clearRect(0,0,canvas.width,canvas.height);
     }
 
-    function drawSheet(canvas) {
+    function draw(ctx){
+        cxt.drawImage(img, left_image_offset, top_image_offset);
+    };
+
+    function drawSheetAndImage(canvas, canvas_id) {
         //
+        if (!(canvas_id == "front_canvas" || canvas_id == "back_canvas")) {
+            $log.error("Unrecognised canvas_id:" + canvas_id);
+            return;
+        }
+        //set the background colour
+        var ctx = canvas.getContext("2d");
+        ctx.fillStyle   = canvas_colour; // set canvas background color
+        ctx.fillRect  (0,   0, canvas.width, canvas.height);  // now fill the canvas
+
         var scale = calculateScale(canvas);
         var avaliable_width = canvas.width - (2 * canvas_padding);
         var avaliable_height = canvas.height - (2 * canvas_padding);
-        var calculated_width = $scope._sheet_width * scale;
-        var calculated_height = $scope._sheet_height * scale;
+        var calculated_sheet_width = $scope._sheet_width * scale;
+        var calculated_sheet_height = $scope._sheet_height * scale;
 
-        var left_offset = canvas_padding + ((avaliable_width - calculated_width) / 2);
-        var top_offset = canvas_padding + ((avaliable_height - calculated_height) / 2);
+        var calculated_overlap = $scope._options_horizontal_overlap * scale;
 
-        var ctx = canvas.getContext("2d");
+        var left_sheet_offset = canvas_padding + ((avaliable_width - calculated_sheet_width) / 2);
+        var top_sheet_offset = canvas_padding + ((avaliable_height - calculated_sheet_height) / 2);
+
         $log.debug(scale);
         $log.debug(avaliable_width);
         $log.debug(avaliable_height);
-        $log.debug(left_offset);
-        $log.debug(top_offset);
-        $log.debug(calculated_width);
-        $log.debug(calculated_height);
-
-        ctx.rect(left_offset, top_offset, calculated_width, calculated_height);
+        //$log.debug(left_offset);
+        //$log.debug(top_offset);
+        $log.debug(calculated_sheet_width);
+        $log.debug(calculated_sheet_height);
+        // drawing the sheet
+        ctx.rect(left_sheet_offset, top_sheet_offset, calculated_sheet_width, calculated_sheet_height);
+        ctx.lineWidth=3;
+        ctx.strokeStyle="Black";
         ctx.stroke();
         ctx.fillStyle="LightGoldenRodYellow";
         ctx.fill();
+
+        //calculate the position of the image or hole
+        var calculated_image_width = $scope._image_width * scale;
+        var calculated_image_height = $scope._image_height * scale;
+
+        var left_image_offset = left_sheet_offset + ((calculated_sheet_width - calculated_image_width)/2) + calculated_overlap;
+        var top_image_offset = top_sheet_offset + ((calculated_sheet_height - calculated_image_height)/2) + calculated_overlap;
+
+        //take the bottom weighting into account
+        top_image_offset = top_image_offset - ($scope._options_bottom_weight * scale);
+
+        if (canvas_id == "front_canvas") {
+            //now put a picture in
+            var cxt = canvas.getContext("2d");
+            // http://placekitten.com/g/200/300
+            var img=new Image();
+            img.src='http://placekitten.com/' + parseInt(calculated_image_width)  + '/' + parseInt(calculated_image_height);
+            img.onload = function(ctx){
+                $log.debug('onload finished');
+                $log.debug(cxt);
+                $log.debug(left_image_offset);
+                $log.debug(parseInt(left_image_offset));
+                $log.debug(top_image_offset);
+                cxt.drawImage(img, left_image_offset, top_image_offset,
+                    calculated_image_width, calculated_image_height);
+            };
+            // TODO: need to mark where the gradient is
+            ctx.lineWidth = calculated_overlap;
+            ctx.strokeStyle = "OrangeRed";
+            ctx.strokeRect(left_image_offset - calculated_overlap, 
+                top_image_offset - calculated_overlap,
+                calculated_image_width + (calculated_overlap * 2),
+                calculated_image_height + (calculated_overlap * 2));
+            //ctx.stroke();
+        } else {
+            // TODO: the space is just a void, size of the image
+            $log.debug("drawing hole");
+            /*ctx.rect(left_image_offset,
+                top_image_offset,
+                calculated_image_width,
+                calculated_image_height);*/
+            //ctx = canvas.getContext("2d");
+            ctx.fillStyle = canvas_colour;
+            ctx.fillRect(left_image_offset,
+                top_image_offset,
+                calculated_image_width,
+                calculated_image_height);
+            //ctx.fillStyle=canvas_colour;
+            //ctx.fill();
+            /*$log.debug(left_image_offset);
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = "Black";
+            ctx.stroke();*/
+        }
     }
 
     $scope.updateCanvas = function() {
@@ -116,12 +186,12 @@ angular.module('myApp.controllers', [])
         // make our canvasses as wide as they can be
         var results_div = document.getElementById('resultsDiv');
 
-        ['front_canvas'].map( function(canvas_id) {
+        ['front_canvas', 'back_canvas'].map( function(canvas_id) {
             $log.debug(canvas_id);
             var canvas = document.getElementById(canvas_id);
             canvas.width = results_div.offsetWidth;
             clearCanvas(canvas);
-            drawSheet(canvas);
+            drawSheetAndImage(canvas, canvas_id);
         });
         /*var canvas = document.getElementById('front_canvas');
         var ctx=canvas.getContext("2d");
